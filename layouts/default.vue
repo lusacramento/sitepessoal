df268dfa0a2baf0f8c429968357786d10cbcff25
lusacramento <lucianossacramento@yahoo.com.br>
2022-06-25 17:56:43 -0300

    comecando do zero. implantado botoes opcao temas

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,50 +1,15 @@
 <template>
-  <div id="app" class="image-bg-1">
-    <div id="header" class="d-flex">
-      <Nav :test="imgBg" />
-    </div>
+  <div id="app" class="">
     <main id="main" class="router-view d-flex">
       <Nuxt />
     </main>
-    <div id="footer" class="d-flex">
-      <Footer />
-    </div>
   </div>
 </template>
 
 <script>
-import Nav from '@/components/nav/Nav'
-import Footer from '@/components/footer/Footer'
 export default {
   name: 'DefaultLayout',
-  components: { Nav, Footer },
-
-  data() {
-    return {
-      imgBg: 0,
-    }
-  },
-  created() {
-    this.swapBg()
-  },
-  methods: {
-    swapBg() {
-      setInterval(() => {
-        this.imgBg++
-      }, 4000)
-    },
-  },
 }
 </script>
 
-<style>
-#app {
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-}
-
-.image-bg-1 {
-  background-image: url('/fone-lu-sacramento.jpg');
-}
-</style>
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,109 @@
 <template>
-  <div id="home-page" class="container-fluid row-big">
-    <div class="row row-big align-content-center">
-      <div class="col-7 col-lg-8"></div>
-      <div class="col-5 col-lg-4 bg-col animate__animated animate__backInRight">
-        <div class="font-white py-1 pt-5">
-          <a href="/music">MUSIC</a>
-        </div>
-        <div class="font-white py-1">
-          <a href="/web-dev"> WEB<br />DEV</a>
-        </div>
-        <div class="font-black py-1">
-          <a href="/contact">CONTACT</a>
-        </div>
-        <div class="font-black pb-5">
-          <a href="/about">ABOUT</a>
-        </div>
-      </div>
+  <div
+    id="home-page"
+    class="container-fluid row-big"
+    :class="{ darkTheme: isDarkMode, lightTheme: !isDarkMode }"
+  >
+    <div class="icontheme">
+      <img
+        class="img"
+        :src="activedTheme.url"
+        :alt="activedTheme.alt"
+        @mouseover="activedTheme.url = imageMouseOver(activedTheme)"
+        @mouseleave="activedTheme.url = imageMouseLeave(activedTheme)"
+        @click="activedTheme = imageClick(activedTheme)"
+      />
     </div>
-    <!-- <div
-      class="row row-big align-items-center justify-content-around text-center"
-    >
-      <div class="col-5 menu-font">
-      </div>
-      <div class="col-5 menu-font">
-      </div>
-    </div>
-    <div
-      class="row row-big align-items-center justify-content-around text-center"
-    >
-      <div class="col-5 menu-font">
-      </div>
-    </div> -->
   </div>
 </template>
 
 <script>
+import imgJpgLight from '@/assets/imgs/light.jpg'
+import imgJpgDark from '@/assets/imgs/dark.jpg'
+import imgGifLight from '@/assets/imgs/light.gif'
+import imgGifDark from '@/assets/imgs/dark.gif'
+
 export default {
   name: 'HomePage',
   layout: 'default',
+
+  data() {
+    return {
+      isDarkMode: false,
+
+      themeLight: {
+        url: imgJpgLight,
+        alt: 'light',
+        drop: imgGifDark,
+        undrop: imgJpgLight,
+      },
+
+      themeDark: {
+        url: imgJpgDark,
+        alt: 'dark',
+        drop: imgGifLight,
+        undrop: imgJpgDark,
+      },
+
+      activedTheme: null,
+    }
+  },
+  watch: {
+    activedTheme(newActivedTheme) {
+      return newActivedTheme
+    },
+  },
+
+  created() {
+    if (this.isDarkMode) {
+      this.activedTheme = this.themeDark
+    } else {
+      this.activedTheme = this.themeLight
+    }
+    // eslint-disable-next-line no-console
+    console.log(this.activedTheme)
+  },
+
+  methods: {
+    imageMouseOver(img) {
+      return img.drop
+    },
+    imageMouseLeave(img) {
+      return img.undrop
+    },
+
+    imageClick(img) {
+      if (this.isDarkMode) {
+        img = this.themeLight
+        this.isDarkMode = false
+      } else {
+        this.isDarkMode = true
+        img = this.themeDark
+      }
+      return img
+    },
+  },
 }
 </script>
 
 <style>
-.row-big {
-  height: 72vh;
+.lightTheme {
+  background-color: #fff !important;
 }
 
-.font-black {
-  font-family: Smooch Sans;
-  color: black;
-  /* font-size: 2.5em; */
-  font-size: 2.5em;
+.darkTheme {
+  background-color: #000 !important;
 }
 
-.font-white {
-  font-family: Smooch Sans;
-  color: white;
-  /* font-size: 2.5em; tamanho para cel */
-  font-size: 2.5em;
+.icontheme {
+  display: flex;
+  justify-content: right;
 }
 
-.bg-col {
-  background-color: rgba(0, 0, 0, 0.123);
-  border-radius: 50px 0px 0px 50px;
+.img {
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  border: solid 1px grey;
 }
 </style>
